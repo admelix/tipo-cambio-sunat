aaf488c485932cc58f988f68ab36aed0976470bb
Jose Francisco sakuda castro <jsakuda@Joses-MacBook-Pro.local>
2021-01-15 17:38:28 -0500

    se suben todos los archivos para realizar el paquete

---
+++ setup.py
@@ -0,0 +1,24 @@
+import setuptools
+
+with open('README.md', 'r') as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='FactoringTotal',
+    version='1.0.0',
+    packages=setuptools.find_packages(),
+    url='https://github.com/admelix/tipo-cambio-sunat',
+    license='GNU General Public License v3.0',
+    author='Jose Sakuda',
+    author_email='sakudacastro@gmail.com',
+    description='Toma el tipo de cambio del dia en sunat y, tambien, el tipo de cambio contable de SBS',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    install_requires=[
+        'pandas',
+        'numpy',
+        'lxml',
+        'bs4',
+        'requests'
+    ]
+)
